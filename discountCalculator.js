4b558748e1d1ce10b95d32d249e5afc20839af4d
Easton Hans <hanseaston697@gmail.com>
2026-03-18 12:14:36 +0300

    Debugging Lab Update

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,14 +1,20 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
+    debugger;
     for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
+    debugger;
 
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
+    debugger;
 
     return totalPrice;
 }
+
+console.log(calculateDiscountedPrice(10, 5));
 
 module.exports = calculateDiscountedPrice;
